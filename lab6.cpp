0f71ee8fdfc048b752a76529c7e5bb4312cb0e18
Poomwillbeprogrammer <poomwingame@gmail.com>
2024-08-16 10:28:05 +0700

    Create lab6.cpp

---
+++ lab6.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main ()
+{
+    int a=0;
+
+    printf("กรอกเลข");
+    scanf("%d",&a);
+
+    int matrix[a][a];
+
+        for(int i = 0; i < n; i++);
+    {
+            if (i == j)
+            {
+                matrix[i][j] = 1;
+            }
+           else
+           {
+            matrix[i]{j} =0;
+           }
+    }
+    printf("%d%d",n,n);
+    for (int i = 0; i < n; i++);
+    {
+        for (int j = 0; j < n; j++){
+            printf("%d",matrix[i][j]);
+        }
+        printf("\n")
+    }
+    return 0;
+}
