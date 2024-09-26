c406498e4febd5addc0519ea9027ce6035329748
Poomwillbeprogrammer <poomwingame@gmail.com>
2024-09-27 03:13:50 +0700

    updatelab10and11
    updatelab10and11

--- Lab10.cpp
+++ Lab10.cpp
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 int main ()
-{
+{//start main
     char word [100] ;
-    char restart [3] ;
     int i , palindrome = 1 ;
 
-    printf ("Enter a word\n") ;
-    scanf ("%s", word ) ;
+    printf ( "Enter a word\n" ) ;
+    scanf ( "%s" , word ) ;
 
-    size_t length = strlen(word); // ใช้ size_t เพื่อเก็บขนาด (size) ของอาเรย์ จะไม่นับตัวเลขแต่จะนับตัวอักษรและ spacebar
+    size_t length = strlen (word) ; // ใช้ size_t เพื่อเก็บขนาด (size) ของอาเรย์ จะไม่นับตัวเลขแต่จะนับตัวอักษรและ spacebar
 
-    printf("Length of the string is: %zu\n", length); 
-    for ( i=0 ; i <  length /2 ; i++ )
+    printf( "Length of the string is: %zu \n" , length ) ; 
+    for ( i=0 ; i < length /2 ; i++ )
     {
-        if (word[i] != word[length - 1 - i])
+        if ( word[i] != word[length - 1 - i] )
         {
-            if(word[i] != word[length - i - 1])
+            if( word[i] != word[length - i - 1] )
             palindrome = 0 ;
             break;
-        }
-    }
+        }//end if
+    }//end for
     
-    if (palindrome == 1) 
+    if ( palindrome == 1 ) 
     {
-        printf("pass") ;
-    } else
+        printf( "pass" ) ;
+    }//end if
+    else
     {
-        printf("not pass") ;
-    }
-    return 0;
+        printf( "not pass" ) ;
+    }//end else
+    return 0 ;
 }//end main
--- Lab11.cpp
+++ Lab11.cpp
@@ -1,52 +1,52 @@
 #include <stdio.h>
 
 void isArmstrong(int num)
-{
-    int originalNum, remainder, result = 0, n = 0;
-    originalNum = num;
-
+{ // start void isArmstong
+    int originalNum , remainder , result = 0 , n = 0 ;
+    originalNum = num ;
     // นับจำนวนหลักของตัวเลข
-    while (originalNum != 0)
+    while ( originalNum != 0 )
     {
-        originalNum /= 10;
-        n += 1;
-    }
+        originalNum /= 10 ;
+        n += 1 ;
+    } // end while
 
-    originalNum = num;
+    originalNum = num ; // นำเลขที่กรอกใส่ลงตัวแปร originalNum
 
     // คำนวณผลรวมของแต่ละหลักที่ยกกำลังจำนวนหลัก
-    while (originalNum != 0)
+    while ( originalNum != 0 )
     {
-        remainder = originalNum % 10;
+        remainder = originalNum % 10 ;
 
         // ยกกำลัง
-        int power = 1;
-        for (int i = 0; i < n; i++)
+        int power = 1 ;
+        for ( int i = 0 ; i < n ; i++ )
         {
-            power *= remainder;
-        }
+            power *= remainder ;
+        }//end fo
 
-        result += power;
-        originalNum /= 10;
-    }
-    
-    if (result == num)
+        result += power ; // นำเลขที่ยกกำลังแล้วใส่ลงตัวแปรresult
+        originalNum /= 10 ;
+    } // end while
+
+    if ( result == num )
     {
-        printf("Pass.\n");
-    }
+        printf( "Pass.\n" ) ;
+    }//end for
     else
     {
-        printf("Not Pass.\n");
-    }
-}
-    int main()
-    {
+        printf("Not Pass.\n") ;
+    }//end else
+}//end function
+int main ()
+    {//start main
         int num ;
         // รับค่าตัวเลขจากผู้ใช้
-        printf("Enter Number: ");
-        scanf("%d", &num);
+        printf ( "Enter Number: " ) ;
+        scanf( "%d" , &num );
 
-        isArmstrong(num);
+        isArmstrong(num);//go to isArmstrong Function
 
-        return 0;
-    }
+        return 0 ;
+    }//end main
+
