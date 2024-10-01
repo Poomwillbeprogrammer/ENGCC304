f174b0a0d210aeac8f4c5b95ac19994bd1743bec
Poomwillbeprogrammer <poomwingame@gmail.com>
2024-10-01 16:40:42 +0700

    Create Lab12.cpp

---
+++ Lab12.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student 
+{
+    char Name[20];
+    char ID[5];
+    float ScoreSub1;
+    float ScoreSub2;
+    float ScoreSub3;
+    float ScoreSub4;
+    float ScoreSub5;
+};
+
+
+int main ()
+{
+    int N = 3 ;
+    struct Student GroupA[ N ] ;
+    printf("Name: %s\n" GroupA[ 0 ].Name)
+    
+
+    return 0 ;
+}//end main 
